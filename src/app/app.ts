e40b3399348f32a5faaa87f36642363bcacdfc11
Laure Nodem <laure.nodem@afric.group>
2026-04-17 02:37:19 +0100

    content of about and persist nav

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Home } from './components/home/home';
+import { Projects } from './components/projects/projects';
+import { Skills } from './components/skills/skills';
+import { Pictures } from './components/pictures/pictures';
+import { Contact } from './components/contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar,Home],
+  imports: [Navbar,Home,Projects,Skills,Pictures,Contact],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
